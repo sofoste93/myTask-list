4b9c31ccaba5eaf2ff1531f40bd3e4f2eefbcab8
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-11-08 18:23:50 +0100

    Create Notes Readme.js

---
+++ task-manager/src/component/Readme.js
@@ -0,0 +1,25 @@
+/*
+
+============================= Notes: Create a react static app =============================
+
+- Pour utiliser un fichier CSS dans un composant React, on utilise la commande import .
+- Pour définir la route dans un lien avec React Router, on utilise le composant NavLink .
+- Avec React Router, on définit les routes possibles dans un composant Switch .
+- On peut changer de composant selon le bouton pressé dans la page HTML.
+- Pour permettre à un composant React de renvoyer un seul élément, on peut utiliser le composant React Fragment .
+- Pour importer un composant, on utilise le mot-clé import .
+- Pour rendre disponible un composant dans un module, il faut l'exporter .
+- Pour utiliser des jeux d'icônes, on peut utiliser le paquet npm react-icons .
+- ReactDOM.render() prend en premier paramètre le nom du composant à afficher .
+- Create React App crée un div dans le fichier HTML nommé root .
+- Pour obtenir les dépendances de React, on peut installer le paquet create-react-app avec npm.
+
+*/
+
+
+
+/*
+
+============================= Notes: Create a react dynamic app =============================
+
+ */
